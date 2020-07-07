aada2b67cd1e943a9b8da7723e2c1a3ff5003605
Thomas Tendyck <tt@edgeless.systems>
2020-07-07 11:58:53 +0200

    readd clock_gettime

--- src/time/edgeless_linux_amd64.go
+++ src/time/edgeless_linux_amd64.go
@@ -9,6 +9,7 @@ package time
 
 /*
 #include <errno.h>
+#include <time.h>
 #include <unistd.h>
 
 long libc_syscall(long number, long a1, long a2, long a3, long a4, long a5, long a6) {
@@ -18,8 +19,11 @@ long libc_syscall(long number, long a1, long a2, long a3, long a4, long a5, long
 int* libc_errno(void) {
 	return &errno;
 }
+
+int libc_clock_gettime(clockid_t clockid, struct timespec* tp) { return clock_gettime(clockid, tp); }
 */
 import "C"
 
 //go:cgo_import_static libc_syscall
 //go:cgo_import_static libc_errno
+//go:cgo_import_static libc_clock_gettime
